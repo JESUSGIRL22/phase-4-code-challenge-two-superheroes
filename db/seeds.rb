f178852651c3e9ef5be23b0c8bd1c93135b20bc3
techlover <e.rono@outlook.com>
2023-09-05 20:16:11 +0300

    progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+kamala = Hero.create(name: 'Kamala Khan', super_name: 'Ms. Marvel')
+squirrel = Hero.create(name: 'Doreen Green', super_name: 'Squirrel Girl')
+gwen = Hero.create(name: 'Gwen Stacy', super_name: 'Spider-Gwen')
+
+strength_power = Power.create(name: 'Super Strength', description: 'Gives the wielder super-human strengths')
+flight_power = Power.create(name: 'Flight', description: 'Gives the wielder the ability to fly through the skies at supersonic speed')
+
+HeroPower.create(hero: kamala, power: strength_power, strength: 'Strong')
+HeroPower.create(hero: gwen, power: flight_power, strength: 'Average')
